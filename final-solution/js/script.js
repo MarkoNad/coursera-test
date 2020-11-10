8552394371708f83eb505915c67f3f054f98fcf0
MarkoNad <marko.nad123@gmail.com>
2020-11-10 19:11:07 +0100

    Implement the skeleton for menu categories.

--- final-solution/js/script.js
+++ final-solution/js/script.js
@@ -1,6 +1,11 @@
 (function (global) {
 
-  var homeHtml = "snippets/home-snippet.html";
+  var dc = {};
+
+  var homeHtmlUrl = "snippets/home-snippet.html";
+  var menuCategoriesHtmlUrl = "snippets/menu-categories-snippet.html";
+  var menuCategoryHtmlUrl = "snippets/menu-category-snippet.html";
+  var allCategoriesUrl = "https://davids-restaurant.herokuapp.com/categories.json";
 
   function insertHtml(selector, html) {
     var targetElement = document.querySelector(selector);
@@ -15,15 +20,75 @@
     insertHtml(selector, html);
   }
 
+  function insertProperty(string, propName, propValue) {
+    var propToReplace = "{{" + propName + "}}";
+    string = string
+      .replace(new RegExp(propToReplace, "g"), propValue);
+    return string;
+  }
+
+
+
   document.addEventListener("DOMContentLoaded", function (event) {
     showLoading("#main-content");
     $ajaxUtils.sendGetRequest(
-      homeHtml,
-      function (responseText) {
-        document.querySelector("#main-content")
-          .innerHTML = responseText;
-      },
+      homeHtmlUrl,
+      receiveMainContent,
       false);
     });
+
+  function receiveMainContent(homeHtml) {
+    insertHtml("#main-content", homeHtml);
+  }
+
+
+
+  dc.loadMenuCategories = function () {
+    showLoading("#main-content");
+
+    $ajaxUtils.sendGetRequest(
+      menuCategoriesHtmlUrl,
+      receiveMenuCategories,
+      false
+    );
+
+  };
+
+  function receiveMenuCategories(menuCategoriesHtml) {
+    $ajaxUtils.sendGetRequest(
+      menuCategoryHtmlUrl,
+      receiveMenuCategory,
+      false
+    );
+
+    function receiveMenuCategory(menuCategoryHtml) {
+      console.log("Receiving menu category.");
+
+      $ajaxUtils.sendGetRequest(
+        allCategoriesUrl,
+        receiveCategoriesArray,
+        true
+      );
+
+      function receiveCategoriesArray(categoriesArray) {
+        console.log("Categories array: " + categoriesArray);
+
+        menuCategoriesHtml += '<div id="categories-container" class="row">';
+
+        for (var i = 0; i < categoriesArray.length; i++) {
+          console.log("Name " + i + ": " + shortName);
+          var shortName = categoriesArray[i].short_name;
+          var populatedCategoryHtml = insertProperty(menuCategoryHtml, 'short_name', shortName);
+          menuCategoriesHtml += populatedCategoryHtml;
+        }
+
+        menuCategoriesHtml += '</div>';
+
+        insertHtml("#main-content", menuCategoriesHtml);
+      }
+    }
+  }
+
+  global.$dc = dc;
 
 })(window);
