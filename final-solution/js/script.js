ebf499abeaf8e0757ddff80a15914ba610bfdfe7
MarkoNad <marko.nad123@gmail.com>
2020-11-11 18:59:41 +0100

    Implement the skeleton for menu categories.

--- final-solution/js/script.js
+++ final-solution/js/script.js
@@ -6,6 +6,9 @@
   var menuCategoriesHtmlUrl = "snippets/menu-categories-snippet.html";
   var menuCategoryHtmlUrl = "snippets/menu-category-snippet.html";
   var allCategoriesUrl = "https://davids-restaurant.herokuapp.com/categories.json";
+  var menuItemsHtmlUrl = "snippets/menu-items-snippet.html";
+  var menuItemHtmlUrl = "snippets/menu-item-snippet.html";
+  var menuItemsUrl = "https://davids-restaurant.herokuapp.com/menu_items.json?category=";
 
   function insertHtml(selector, html) {
     var targetElement = document.querySelector(selector);
@@ -93,6 +96,73 @@
       }
     }
   }
+
+
+
+
+
+
+
+dc.loadMenuItems = function (shortName) {
+    showLoading("#main-content");
+    console.log("Category short name: " + shortName);
+
+    $ajaxUtils.sendGetRequest(
+      menuItemsHtmlUrl,
+      receiveMenuItems,
+      false
+    );
+
+    function receiveMenuItems(menuItemsHtml) {
+      console.log("Category short name in receiveMenuItems: " + shortName);
+      $ajaxUtils.sendGetRequest(
+        menuItemHtmlUrl,
+        receiveMenuItem,
+        false
+      );
+
+      function receiveMenuItem(menuItemHtml) {
+        console.log("Receiving menu item.");
+        console.log("Category short name in receiveMenuItem: " + shortName);
+
+        $ajaxUtils.sendGetRequest(
+          menuItemsUrl + shortName,
+          receiveItemsArray,
+          true
+        );
+
+        function receiveItemsArray(itemsArrayContainer) {
+          console.log("Items array: " + itemsArrayContainer);
+
+          menuItemsHtml += '<div id="menu-items-container" class="row">';
+
+          var itemsArray = itemsArrayContainer.menu_items;
+
+          for (var i = 0; i < itemsArray.length; i++) {
+            var shortName = itemsArray[i].short_name;
+            console.log("Name " + i + ": " + shortName);
+
+            var categoryName = itemsArrayContainer.category.short_name;
+            var itemName = itemsArray[i].name;
+            var itemShortName = itemsArray[i].short_name;
+            var description = itemsArray[i].description;
+            var price = itemsArray[i].price_small;
+
+            var populatedItemHtml = insertProperty(menuItemHtml, 'item_short_name', itemShortName);
+            populatedItemHtml = insertProperty(populatedItemHtml, 'item_name', itemName);
+            populatedItemHtml = insertProperty(populatedItemHtml, 'category_name', categoryName);
+            
+            menuItemsHtml += populatedItemHtml;
+          }
+
+          menuItemsHtml += '</div>';
+
+          insertHtml("#main-content", menuItemsHtml);
+        }
+      }
+    }
+  };
+
 
   global.$dc = dc;
 
