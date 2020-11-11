5038c9f947bf31d62132993bd043d3c63374dbd4
MarkoNad <marko.nad123@gmail.com>
2020-11-11 20:56:12 +0100

    Fix navigation buttons activation.

--- final-solution/js/script.js
+++ final-solution/js/script.js
@@ -46,7 +46,22 @@
 
 
 
+  var switchMenuToActive = function () {
+    // Remove 'active' from home button
+    var classes = document.querySelector("#navHomeButton").className;
+    classes = classes.replace(new RegExp("active", "g"), "");
+    document.querySelector("#navHomeButton").className = classes;
+
+    // Add 'active' to menu button if not already there
+    classes = document.querySelector("#navMenuButton").className;
+    if (classes.indexOf("active") == -1) {
+      classes += " active";
+      document.querySelector("#navMenuButton").className = classes;
+    }
+  }
+
   dc.loadMenuCategories = function () {
+    switchMenuToActive();
     showLoading("#main-content");
 
     $ajaxUtils.sendGetRequest(
